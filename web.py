689bd56b6ba03b7511332f6491aabb9244a57965
jeffthemaximum <frey.maxim@gmail.com>
2015-10-28 14:22:57 -0400

    bootstrap errors

--- web.py
+++ web.py
@@ -1,8 +1,8 @@
 from app import app
 from flask.ext.bootstrap import Bootstrap
 
-bootstrap = Bootstrap()
-bootstrap.init_app(app)
+Bootstrap(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
